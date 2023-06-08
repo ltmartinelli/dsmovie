27a287c10b220bc7cffc4ad970a345d017d05555
ltmartinelli <lucastrindademartinelli@gmail.com>
2023-06-08 16:51:04 -0300

    Props and Dynamic Listing

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -11,33 +11,53 @@ export default function Listing()
 
   const [pageNumber, setPageNumber] = useState<number>(0);
 
+  const [page, setPage] = useState<MoviePage>({
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 12,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true,
+  });
+
   useEffect(() =>
   {
-    axios.get(`${BASE_URL}/movies?size=12&page=1`)
+    axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
       .then(response =>
       {
         const data = response.data as MoviePage
-        setPageNumber(data.number)
+        setPage(data)
       })
-  }, [])
+  }, [pageNumber])
+
+
+  const movie = {
+    id: 1,
+    image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+    title: "The Witcher",
+    count: 2,
+    score: 4.5
+  };
+
 
   return (
     <>
       <Pagination />
       <div className='container'>
         <div className='row'>
-          <div className='col-sm-6 col-lg-4 col-xl-3 mb-3'>
-            <MovieCard />
-          </div>
-          <div className='col-sm-6 col-lg-4 col-xl-3 mb-3'>
-            <MovieCard />
-          </div>
-          <div className='col-sm-6 col-lg-4 col-xl-3 mb-3'>
-            <MovieCard />
-          </div>
-          <div className='col-sm-6 col-lg-4 col-xl-3 mb-3'>
-            <MovieCard />
-          </div>
+          {
+            page.content.map((movie) =>
+            {
+              return (
+                <div key={movie.id} className='col-sm-6 col-lg-4 col-xl-3 mb-3'>
+                  <MovieCard movie={movie} />
+                </div>
+              );
+            })
+          }
         </div>
       </div>
     </>
