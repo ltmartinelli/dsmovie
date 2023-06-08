b3c707977a4791e0f2fbc2b9474666cdad246580
ltmartinelli <lucastrindademartinelli@gmail.com>
2023-06-08 11:43:01 -0300

    useState and useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from 'react';
 import MovieCard from '../../components/MovieCard';
 import Pagination from '../../components/Pagination';
 import './styles.css'
+import { BASE_URL } from '../../utils/requests';
+import axios from 'axios';
+import { MoviePage } from '../../models/movie';
 
 export default function Listing()
 {
+
+  const [pageNumber, setPageNumber] = useState<number>(0);
+
+  useEffect(() =>
+  {
+    axios.get(`${BASE_URL}/movies?size=12&page=1`)
+      .then(response =>
+      {
+        const data = response.data as MoviePage
+        setPageNumber(data.number)
+      })
+  }, [])
+
   return (
     <>
       <Pagination />
