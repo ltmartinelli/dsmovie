3fee966277811c2899ceb08b20e5f114fe7c3691
ltmartinelli <lucastrindademartinelli@gmail.com>
2023-06-07 21:59:41 -0300

    Port and Host Fix for Railway

--- backend/src/main/java/com/devsuperior/dsmovie/DsmovieApplication.java
+++ backend/src/main/java/com/devsuperior/dsmovie/DsmovieApplication.java
@@ -2,12 +2,24 @@ package com.devsuperior.dsmovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class DsmovieApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DsmovieApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(DsmovieApplication.class);
+        Environment env = app.run(args).getEnvironment();
+
+        String port = env.getProperty("PORT");
+        if (port == null || port.isEmpty()) {
+            port = "8080"; // Set a default port if the PORT environment variable is not provided
+        }
+
+        String host = "0.0.0.0"; // The host to bind to, allowing connections from any IP address
+
+        String serverUrl = "http://" + host + ":" + port;
+        System.out.println("Server running at " + serverUrl);
+    }
 
 }
