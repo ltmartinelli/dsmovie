ab66658e2cba9a21ea2a9d89ea3cba752959d280
ltmartinelli <lucastrindademartinelli@gmail.com>
2023-06-08 17:57:03 -0300

    Show Score

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -3,15 +3,54 @@ import StarFull from '../../assets/img/star-full.svg';
 import StarHalf from '../../assets/img/star-half.svg';
 import StarEmpty from '../../assets/img/star-empty.svg';
 
-export default function MovieStars()
+type Props = {
+    score: number
+}
+
+type StarProps = {
+    fill: number
+}
+
+function getFills(score: number)
 {
+
+    const fills = [0, 0, 0, 0, 0];
+
+    const integerPart = Math.floor(score);
+
+    for (let i = 0; i < integerPart; i++)
+    {
+        fills[i] = 1;
+    }
+
+    const diff = score - integerPart;
+    if (diff > 0)
+    {
+        fills[integerPart] = 0.5;
+    }
+
+    return fills;
+}
+
+
+function Star({ fill }: StarProps)
+{
+    if(fill === 0){return <img src={StarEmpty} alt="" />}
+    else if(fill === 1){return <img src={StarFull} alt="" />}
+    else{return <img src={StarHalf} alt="" />}
+}
+
+export default function MovieStars({ score }: Props)
+{
+    const fills = getFills(score);
+
     return (
         <div className="dsmovie-stars-container">
-            <img src={StarFull} alt="" />
-            <img src={StarFull} alt="" />
-            <img src={StarFull} alt="" />
-            <img src={StarHalf} alt="" />
-            <img src={StarEmpty} alt="" />
+            <Star fill={fills[0]} />
+            <Star fill={fills[1]} />
+            <Star fill={fills[2]} />
+            <Star fill={fills[3]} />
+            <Star fill={fills[4]} />
         </div>
     );
 }
